b740e311ddb0241a7ac4f01ebf1e5de0d1c1aafc
rongjia <1085879662@qq.com>
2019-11-06 20:35:01 +0800

    '2'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,9 +25,29 @@
 //     ],
 // };
 
-let sidebar = [
+let sidebar = {
+    '/about/': [
+        {
+            title: 'JavaScript', // 侧边栏名称
+            collapsable: true, // 可折叠
+            children: [
+                // ''空字符串代表主页，显示README.md中的内容
+                '',
+                '/about/first'
+                //['first', '111111111111111111'],//使用数组为侧边栏起别名，前边是md名称，后边是链接显示的文字
 
-]
+            ]
+        },
+        {
+            title: 'css', // 侧边栏名称
+            collapsable: true, // 可折叠
+            children: [
+                '/about/first', // 你的md文件地址
+                '/about/two'
+            ]
+        },
+    ],
+}
 
 module.exports = {
     title: '个人主页',
@@ -37,26 +57,28 @@ module.exports = {
         ['link', { rel: 'manifest', href: '/manifest.json' }],
     ],
     themeConfig: {
-        sidebarDepth: 4,
+        //sidebarDepth: 4,//这是嵌套标题链接，自动显示当前激活（导航）页面标题的链接，即显示深度（h1-h6的深度）
         lastUpdated: 'Last Updated',// 文档更新时间：每个文件git最后提交的时间
+        displayAllHeaders: true,// 侧边栏只会显示由当前活动页面的标题（headers）组成的链接，你可以将 themeConfig.displayAllHeaders 设置为 true 来显示所有页面的标题链接：
         // 顶部导航栏
         nav: [
             { text: '主页', link: '/' },
             {
                 text: '博文',
                 items: [
-                    { text: 'Android', link: '/android/' },
-                    { text: 'Thought', link: '/Thought/' },
+                    { text: 'About', link: '/about/' },
+                    { text: 'Study', link: '/study/' },
                     { text: 'Web', link: '/web/' }
                 ]
             },
             { text: '关于', link: '/about/' },
             { text: 'Github', link: 'https://www.github.com/codeteenager' },
         ],
-        // 侧边栏菜单( 一个模块对应一个菜单形式 )
+        // 侧边栏菜单( 一个模块对应一个菜单形式 ) ,为以下路由添加侧边栏
+        // sidebar: ['/', '/about/', '/study/'],
+        // 添加侧边栏,该语法表示使用当前页面标题自动生成侧边栏
+        // sidebar: 'auto',
         sidebar: sidebar,
-        // 添加侧边栏
-        sidebar: 'auto',
 
     },
     configureWebpack: {
